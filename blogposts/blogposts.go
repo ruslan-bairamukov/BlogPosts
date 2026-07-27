b240b8b6b27f6ea6d8be9d7ce12dcc405065854d
ruslan-bairamukov <rusbay.ts@gmail.com>
2026-07-27 20:00:32 +0300

    add helper funcs: getPost, newPost

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -1,17 +1,50 @@
 package blogposts
 
 import (
+	"io"
 	"io/fs"
 )
 
-type Post struct{}
+type Post struct {
+	Title string
+}
 
-func NewPostsFromFS(fileSystem fs.FS) []Post {
-	dir, _ := fs.ReadDir(fileSystem, ".")
-	posts := make([]Post, 0, 2)
-	for range dir {
-		posts = append(posts, Post{})
+func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(fileSystem, ".")
+	if err != nil {
+		return nil, err
 	}
 
-	return posts
+	posts := make([]Post, 0, 2)
+
+	for _, f := range dir {
+		post, err := getPost(fileSystem, f.Name())
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+
+	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	return newPost(postFile)
+}
+
+func newPost(postFile io.Reader) (Post, error) {
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+
+	return post, nil
 }
